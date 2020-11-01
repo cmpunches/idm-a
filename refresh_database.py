f0d8c129cb95f84f07aebdf1ebb09a533058899d
Chris Punches <bagira@forge.silogroup.org>
2020-11-01 17:21:08 -0500

    vertderferk

--- refresh_database.py
+++ refresh_database.py
@@ -1,12 +1,7 @@
-import werkzeug
-werkzeug.cached_property = werkzeug.utils.cached_property
-
-from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from UserLifecycle.Models.Models import UserModel
 
-api = Api()
 db = SQLAlchemy()
 
 
@@ -21,7 +16,6 @@ app = create_app('config.py')
 
 with app.app_context():
     print("We're in app context.  Stuff should be deleted.")
-    db.init_app( app=app )
     db.drop_all()
     db.session.commit()
 
