b0b73f10deb80a776368b2a2d2d427d0423717e0
Chris Punches <bagira@forge.silogroup.org>
2020-11-01 17:39:02 -0500

    closer

--- refresh_database.py
+++ refresh_database.py
@@ -1,7 +1,9 @@
-from main import db, app, create_app
+from Core.Shared.APP import create_app
+from Core.Shared.DB import create_db
 
 
-# app = create_app('config.py')
+app = create_app('config.py')
+db = create_db( app )
 
 with app.app_context():
     print("We're in app context.  Stuff should be deleted.")
